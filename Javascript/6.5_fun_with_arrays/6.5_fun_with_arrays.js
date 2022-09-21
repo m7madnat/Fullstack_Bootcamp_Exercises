3163a4426998c92fe3658de0a15ea5aae427bbe6
m7madnat <m7madnatseh1999@gmail.com>
2022-09-21 10:06:32 +0300

    Update 6.5_fun_with_arrays.js

--- Javascript/6.5_fun_with_arrays/6.5_fun_with_arrays.js
+++ Javascript/6.5_fun_with_arrays/6.5_fun_with_arrays.js
@@ -1,16 +1,27 @@
-const iterArr = Array.from({length: 100}, (_, i) => i); // The use an ( _ ) character to denote that we don't need the first
+// Fill an array with 100 of the same object using the array fill method.
+let arr1 = new Array(100).fill({name: "Mohamed"});
+console.log(arr1);
+
+//Create an array with numbers ranging from 1-100 using the Array.from method.
+
+const iterArr = Array.from({length: 100}, (_, i) => i ); // The use an ( _ ) character to denote that we don't need the first
 console.log(iterArr);                                  // parameter of the function, which is the value of the array element.  
+
 
 const myObj = {
     one: 1,
     two: 2,
     three: 3,
 };
+
+
 const myArray = Object.values(myObj);
 console.log(myArray);
 
+
 const newObj = Object.assign({}, myArray);
 console.log(newObj);
+
 
 console.log(myArray instanceof Array);
 
